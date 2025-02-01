f756880636e09a8c2383a4cf4260765fc9668718
pavka <pavka665@gmail.com>
2025-02-01 20:36:36 +0200

    Adding in ToDo App v2

--- JavaScript-Simple-Projects/todo-app/v2/app.js
+++ JavaScript-Simple-Projects/todo-app/v2/app.js
@@ -3,65 +3,81 @@ const todoForm = document.getElementById('todo-form');
 const todoInput = document.getElementById('todo-input');
 const todoList = document.getElementById('todo-list');
 
-// Массив для зранения задач
+// Кнопка фильтрации
+const filterButtons = document.querySelectorAll('.filter-btn');
+let filter = 'all';
+
+// Массив фильтрации
 let todos = [];
 
-// Добовление задач
+// Добавление задач
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    
+
     const task = todoInput.value.trim();
-    if (task === '') return; // Игнорируем пустую строку
-    
-    // Добовляем задачу в массив
+    if (task === '') return;
+
     const newTask = {id: Date.now(), text: task, completed: false};
     todos.push(newTask);
 
-    // Оображение
     renderTodos();
     saveToLocalStorage();
 
-    // Очищаем поле ввода
     todoInput.value = '';
 });
 
-// Отображение задач
+// Отображение задачи
 function renderTodos() {
-    todoList.innerHTML = ''; // Очищаем список
+    todoList.innerHTML = '';
 
-    todos.forEach((todo) => {
+    // Фильтрация завач перед рендеринга
+    const filteredTodos = todos.filter((todo) => {
+        if (filter === 'all') return true;
+        if (filter === 'active') return !todo.completed;
+        if (filter === 'completed') return todo.completed;
+    });
+    
+    filteredTodos.forEach((todo) => {
         const li = document.createElement('li');
         li.className = 'flex items-center justify-between bg-gray-100 p-2 rounded shadow';
 
-        // Статус задачи
         const textSpan = document.createElement('span');
         textSpan.textContent = todo.text;
-        textSpan.className = `flex-1 ${todo.completed ? 'line-through text-gray-500' : ''}`;
+        textSpan.className = `flex-1 ${ todo.completed ? 'line-through text-gray-500' : '' }`;
         textSpan.addEventListener('click', () => toggleCompleted(todo.id));
 
-        // Удаление задачи
         const deleteBtn = document.createElement('button');
         deleteBtn.textContent = 'Delete';
         deleteBtn.className = 'text-red-500 hover:underline';
-        deleteBtn.addEventListener('click', () => deleteTask(todo.id))
+        deleteBtn.addEventListener('click', () => deleteTask(todo.id));
 
         li.appendChild(textSpan);
         li.appendChild(deleteBtn);
-        todoList.appendChild(li);
+        todoList.append(li);
     });
 }
 
-// Сохрарнение в localStorage
+// Обработка кликов по фильтрам
+filterButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        filterButtons.forEach((btn) => btn.classList.remove('bg-opacity-50'));
+        e.target.classList.add('bg-opacity-50');
+        filter = e.target.id.replace('filter-', '');
+        renderTodos();
+    })
+})
+
+// Сохранение в Localstorage
 function saveToLocalStorage() {
     localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-// Загрузка из localStorage
+// Загрузка из Localstorage
 function loadFromLocalStorage() {
     const storedTodos = localStorage.getItem('todos');
     if (storedTodos) {
         todos = JSON.parse(storedTodos);
-        renderTodos()
+        renderTodos();
     }
 }
 
